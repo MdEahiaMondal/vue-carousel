d97fd426b186dda30eaf3337c27ee7003a2cea01
git-e-001 <git-001@devxhub.com>
2022-02-15 23:00:16 +0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <carousel
+    :navigation="true"
+    :pagination="true"
+    :startAutoPlay="true"
+    :timeout="10000"
+    v-slot="{ currentSlide }"
+  >
+    <slider v-for="(slide, index) in sliders" :key="index">
+      <div v-show="currentSlide === index + 1">
+        <img :src="slide" alt="" />
+      </div>
+    </slider>
+  </carousel>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { reactive } from "@vue/reactivity";
+import Carousel from "./components/Carousel.vue";
+import Slider from "./components/Slider.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Carousel,
+    Slider,
+  },
+  setup() {
+    const sliders = reactive([
+      "../assets/bg-1.jpg",
+      "../assets/bg-2.jpg",
+      "../assets/bg-3.jpg",
+    ]);
+    return { sliders };
+  },
+};
 </script>
-
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="slider slide-info">
+    <transition name="slide">
+      <slot></slot>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Slider",
+  setup() {},
+};
+</script>
+
+<style lang="scss">
+.slide-info > * {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  max-height: 100%;
+  height: 100%;
+  img {
+    min-width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 0.7s ease-in-out;
+}
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+}
+.slide-enter-to,
+.slide-leave-from {
+  opacity: 1;
+}
+</style>
